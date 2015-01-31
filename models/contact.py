0075c53350caeea4f734bb12a9be42d3604822d2
Jeff Hemphill <jhemphill@fb.com>
2015-01-31 00:57:27 -0800

    Create a bot to facilitate contact

---
+++ models/contact.py
@@ -0,0 +1,119 @@
+import time
+
+from core.citext import CIText
+from core.db import db
+
+
+class Challenge(db.Model):
+    ## States
+    WAITING_FOR_READY = -1
+    COMPLETED = 0
+    # A positive state indicates the number of seconds we will wait for
+    # the challengers
+    COUNTDOWN = 4 # gives us 3... 2... 1... go!
+
+    id = db.Column(db.Integer(), primary_key=True)
+    channel = db.Column(CIText(), unique=True)
+
+    state = db.Column(db.Integer())
+    time = db.Column(db.Integer())
+
+    challenger = db.Column(CIText())
+    challenged = db.Column(CIText())
+    ready = db.column(CIText())
+
+
+    def __init__(self, channel):
+        self.channel = channel
+        self.finish()
+
+
+    def reset(self):
+        self.state = Challenge.COMPLETED
+        self.time = int(time.time())
+        self.challenger = ''
+        self.challenged = ''
+        self.ready = ''
+
+
+    def start(self, challenger, challenged):
+        """Wait for the challenged players to ready up"""
+        self.state = Challenge.WAITING_FOR_READY
+        self.challenger = challenger
+        self.challenged = ' '.join(challenged)
+        self.ready = ''
+
+
+    def set_ready(self, player):
+        if player not in self.challenged:
+            return
+
+        ready = set(self.ready.split())
+        ready.add(player)
+
+        self.ready = ' '.join(ready)
+        self.time = int(time.time())
+
+
+    def is_ready(self):
+        challenged = set(self.challenged.split())
+        ready = set(self.ready.split())
+        return len(ready.intersection(challenged)) >= 2
+
+
+    def start_clock(self):
+        self.state = Challenge.COUNTDOWN
+        self.time = int(time.time())
+
+
+    def tick(self):
+        """Decrement the state if positive"""
+        self.time = int(time.time())
+        if self.state <= 0:
+            return self.state
+
+        self.state -= 1
+        return self.state
+
+
+    @staticmethod
+    def get(channel):
+        """Get the challenge for the given channel (read-only)"""
+
+        challenge = db.session.query(Challenge).filter_by(
+            channel=channel
+        ).first()
+
+        if not challenge:
+            challenge = Challenge(channel)
+            db.session.add(challenge)
+
+        return challenge
+
+
+    class lock:
+        """Checkout the challenge for the given channel (for reading and writing)"""
+        def __init__(self, channel):
+            self.challenge = db.session.query(Challenge).filter_by(
+                channel=channel
+            ).with_lockmode('update').first()
+
+            if not self.challenge:
+                challenge = Challenge(channel)
+                db.session.with_lockmode('update').add(challenge)
+
+        def __enter__(self):
+            return self.challenge
+
+        def __exit__(self, type, value, traceback):
+            db.session.commit()
+
+
+    def __repr__(self):
+        return "%s: %s -> [%s] (state: %s, time: %d)" % (
+            self.channel,
+            self.challenger,
+            self.challenged,
+            self.state,
+            self.time,
+        )
---
+++ plugins/contact.py
@@ -0,0 +1,101 @@
+""" A bot to make the wordkeeper's job a little easier during games of Contact.
+
+wordkeeper> !challenge @player1 @player2
+slaskbot> @player1 @player2: type !ready when here
+player1> !ready
+player2> !ready
+slaskbot> (@wordkeeper) Starting countdown...
+slaskbot> 3
+slaskbot> 2
+slaskbot> 1
+slaskbot> go!
+
+"""
+
+import logging
+import re
+import time
+
+import config
+from models.contact import Challenge
+
+
+# Number of seconds we wait for someone to be ready
+TIMEOUT = 120
+
+
+def on_message(msg, server):
+    channel = server["channel"]
+    if channel not in config.CONTACT_CHANNELS:
+        return
+
+    with Challenge.lock(channel) as challenge:
+        if re.search(r"!contactreset", msg.get("text", "")):
+            challenge.reset(channel)
+            return "contactbot reset"
+
+        state = challenge.state
+        if state == Challenge.COMPLETED:
+            return start_challenge(challenge, msg)
+        elif state == Challenge.WAITING_FOR_READY:
+            return set_ready(challenge, msg)
+        elif state > 0:
+            return tick(challenge)
+        else:
+            challenge.reset()
+            if config.DEBUG:
+                log = logging.getLogger(__name__)
+                log.warn("Channel %s has illegal state %d" % (channel, state))
+            return "sorry guys jeff is not good with computer"
+
+
+def start_challenge(challenge, msg):
+    match = re.findall(r"!challenge (.*)", msg.get("text", ""))
+    if not match:
+        return
+
+    players = [x.lower() for x in match[0].split() if valid_name(x)]
+    if len(players) < 2:
+        return "You need to tag all the people who contacted (at least two)"
+
+    challenge.start_challenge(msg.get("user_name", ""))
+    return "%s: type !ready when here" % (' '.join(players))
+
+
+def valid_name(name):
+    """True iff the name is a valid Slack username
+
+    Slack's draconian naming conventions make it a little less likely
+    that someone will bork the contactbot.
+
+    """
+    return all([c.isalnum() or c in '-_' for c in name])
+
+
+def set_ready(challenge, msg):
+    match = re.search(r"!ready")
+    if not match:
+        if int(time.time()) - challenge.time > TIMEOUT:
+            challenge.reset()
+            return "It's been too long since the last !ready. Giving up."
+        else:
+            return
+
+    challenge.set_ready(msg.get("user_name", ""))
+
+    if challenge.is_ready():
+        challenge.start_clock()
+        return "(%s) Starting countdown..." % (challenge.challenger)
+
+
+def tick(challenge):
+    dt = int(time.time()) - challenge.time
+
+    if 0 <= dt <= 1:
+        time.sleep(1 - dt)
+
+    state = challenge.tick()
+    if state != 0:
+        return "%d..." % state
+    else:
+        return "go!"
